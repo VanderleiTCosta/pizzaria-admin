c920ae1f6d82832769d742d7eac824be5361da14
Vanderlei <derlei.vc@gmail.com>
2025-08-29 15:26:12 -0300

    Ajusta backend para ambiente de produção (PORT e MONGO_URL)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://vanderleicosta:Derley18@projeto.dnwmwqs.mongodb.net/?retryWrites=true&w=majority&appName=Projeto').then(()=>console.log("DB conectado"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB conectado"));
 }
